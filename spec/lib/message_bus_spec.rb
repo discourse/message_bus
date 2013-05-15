e8bfa0c1467d86ffcba97509f5191eca50483040
Sam <sam.saffron@gmail.com>
2013-05-15 16:17:32 +1000

    up timeout, clean test in case this fixes travis

--- spec/lib/message_bus_spec.rb
+++ spec/lib/message_bus_spec.rb
@@ -22,49 +22,49 @@ describe MessageBus do
       data = msg.data
     end
     MessageBus.publish("/chuck", {:norris => true})
-    wait_for(1000){ data }
+    wait_for(2000){ data }
 
     data["norris"].should == true
   end
 
   it "should get a message if it subscribes to it" do
-    @data,@site_id,@channel = nil
+    user_ids,data,site_id,channel = nil
 
     MessageBus.subscribe("/chuck") do |msg|
-      @data = msg.data
-      @site_id = msg.site_id
-      @channel = msg.channel
-      @user_ids = msg.user_ids
+      data = msg.data
+      site_id = msg.site_id
+      channel = msg.channel
+      user_ids = msg.user_ids
     end
 
     MessageBus.publish("/chuck", "norris", user_ids: [1,2,3])
 
-    wait_for(1000){@data}
+    wait_for(2000){data}
 
-    @data.should == 'norris'
-    @site_id.should == 'magic'
-    @channel.should == '/chuck'
-    @user_ids.should == [1,2,3]
+    data.should == 'norris'
+    site_id.should == 'magic'
+    channel.should == '/chuck'
+    user_ids.should == [1,2,3]
 
   end
 
 
   it "should get global messages if it subscribes to them" do
-    @data,@site_id,@channel = nil
+    data,site_id,channel = nil
 
     MessageBus.subscribe do |msg|
-      @data = msg.data
-      @site_id = msg.site_id
-      @channel = msg.channel
+      data = msg.data
+      site_id = msg.site_id
+      channel = msg.channel
     end
 
     MessageBus.publish("/chuck", "norris")
 
-    wait_for(1000){@data}
+    wait_for(2000){data}
 
-    @data.should == 'norris'
-    @site_id.should == 'magic'
-    @channel.should == '/chuck'
+    data.should == 'norris'
+    site_id.should == 'magic'
+    channel.should == '/chuck'
 
   end
 
