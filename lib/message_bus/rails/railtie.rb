6cdab8a4e0d06203cbdb146b52159e710b891a82
Sam <sam.saffron@gmail.com>
2013-11-25 15:20:14 +1100

    engine for serving assets

--- lib/message_bus/rails/railtie.rb
+++ lib/message_bus/rails/railtie.rb
@@ -1,5 +1,8 @@
 module MessageBus; module Rails; end; end
 
+# rails engine for asset pipeline
+class MessageBus::Rails::Engine < ::Rails::Engine; end
+
 class MessageBus::Rails::Railtie < ::Rails::Railtie
   initializer "message_bus.configure_init" do |app|
     MessageBus::MessageHandler.load_handlers("#{Rails.root}/app/message_handlers")
