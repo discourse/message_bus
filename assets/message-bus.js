823a287aaf0fdec28216e405c8ec552c47e1a494
Sam <sam.saffron@gmail.com>
2013-11-29 18:47:20 +1100

    refactor long poller, ensure long polling takes into account the duration it already waited

--- assets/message-bus.js
+++ assets/message-bus.js
@@ -10,7 +10,7 @@
 window.MessageBus = (function() {
   // http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
   var callbacks, clientId, failCount, interval, shouldLongPoll, queue, responseCallbacks, uniqueId, baseUrl;
-  var me, started, stopped;
+  var me, started, stopped, longPoller, pollTimeout;
 
   uniqueId = function() {
     return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
@@ -29,17 +29,68 @@ window.MessageBus = (function() {
   failCount = 0;
   baseUrl = "/";
 
+  /* TODO: The plan is to force a long poll as soon as page becomes visible
+  // MIT based off https://github.com/mathiasbynens/jquery-visibility/blob/master/jquery-visibility.js
+  initVisibilityTracking =  function(window, document, $, undefined) {
+    var prefix;
+    var property;
+    // In Opera, `'onfocusin' in document == true`, hence the extra `hasFocus` check to detect IE-like behavior
+    var eventName = 'onfocusin' in document && 'hasFocus' in document ? 'focusin focusout' : 'focus blur';
+    var prefixes = ['webkit', 'o', 'ms', 'moz', ''];
+    var $event = $.event;
+
+    while ((prefix = prefixes.pop()) !== undefined) {
+      property = (prefix ? prefix + 'H': 'h') + 'idden';
+      var supportsVisibility = typeof document[property] === 'boolean';
+      if (supportsVisibility) {
+        eventName = prefix + 'visibilitychange';
+        break;
+      }
+    }
+
+    $(/blur$/.test(eventName) ? window : document).on(eventName, function(event) {
+      var type = event.type;
+      var originalEvent = event.originalEvent;
+
+      // Avoid errors from triggered native events for which `originalEvent` is
+      // not available.
+      if (!originalEvent) {
+              return;
+      }
+
+      var toElement = originalEvent.toElement;
+
+      // If it’s a `{focusin,focusout}` event (IE), `fromElement` and `toElement`
+      // should both be `null` or `undefined`; else, the page visibility hasn’t
+      // changed, but the user just clicked somewhere in the doc. In IE9, we need
+      // to check the `relatedTarget` property instead.
+      if (
+              !/^focus./.test(type) || (
+                      toElement === undefined &&
+                      originalEvent.fromElement === undefined &&
+                      originalEvent.relatedTarget === undefined
+              )
+      ) {
+          visibilityChanged(property && document[property] || /^(?:blur|focusout)$/.test(type) ? 'hide' : 'show');
+      }
+    });
+
+  };
+  */
+
+  var hiddenProperty;
+
+  $.each(["","webkit","ms","moz","ms"], function(index, prefix){
+    var check = prefix + (prefix === "" ? "hidden" : "Hidden");
+    if(document[check] !== undefined ){
+      hiddenProperty = check;
+    }
+  });
+
   var isHidden = function() {
-    if (document.hidden !== void 0) {
-      return document.hidden;
-    } else if (document.webkitHidden !== void 0) {
-      return document.webkitHidden;
-    } else if (document.msHidden !== void 0) {
-      return document.msHidden;
-    } else if (document.mozHidden !== void 0) {
-      return document.mozHidden;
+    if (hiddenProperty !== undefined){
+      return document[hiddenProperty];
     } else {
-      // problamatic fallback
       return !document.hasFocus;
     }
   };
@@ -48,9 +99,67 @@ window.MessageBus = (function() {
     return me.alwaysLongPoll || !isHidden();
   };
 
+  longPoller = function(poll,data){
+    var gotData = false;
+    var lastAjax = new Date();
+
+    return $.ajax(baseUrl + "message-bus/" + clientId + "/poll?" + (!shouldLongPoll() || !me.enableLongPolling ? "dlp=t" : ""), {
+      data: data,
+      cache: false,
+      dataType: 'json',
+      type: 'POST',
+      headers: {
+        'X-SILENCE-LOGGER': 'true'
+      },
+      success: function(messages) {
+        failCount = 0;
+        $.each(messages,function(_,message) {
+          gotData = true;
+          $.each(callbacks, function(_,callback) {
+            if (callback.channel === message.channel) {
+              callback.last_id = message.message_id;
+              callback.func(message.data);
+            }
+            if (message.channel === "/__status") {
+              if (message.data[callback.channel] !== undefined) {
+                callback.last_id = message.data[callback.channel];
+              }
+            }
+          });
+        });
+      },
+      error: failCount += 1,
+      complete: function() {
+        if (gotData) {
+          pollTimeout = setTimeout(poll, 100);
+        } else {
+          interval = me.callbackInterval;
+          if (failCount > 2) {
+            interval = interval * failCount;
+          } else if (!shouldLongPoll()) {
+            // slowning down stuff a lot when hidden
+            // we will need to add a lot of fine tuning here
+            interval = interval * 4;
+          }
+          if (interval > me.maxPollInterval) {
+            interval = me.maxPollInterval;
+          }
+
+          interval -= (new Date() - lastAjax);
+          if (interval < 100) {
+            interval = 100;
+          }
+
+          pollTimeout = setTimeout(poll, interval);
+        }
+        me.longPoll = null;
+      }
+    });
+  };
+
   me = {
     enableLongPolling: true,
-    callbackInterval: 60000,
+    callbackInterval: 15000,
     maxPollInterval: 3 * 60 * 1000,
     callbacks: callbacks,
     clientId: clientId,
@@ -64,8 +173,7 @@ window.MessageBus = (function() {
 
     // Start polling
     start: function(opts) {
-      var poll,
-        _this = this;
+      var poll;
 
       if (started) return;
       started = true;
@@ -74,7 +182,7 @@ window.MessageBus = (function() {
       if (!opts) opts = {};
 
       poll = function() {
-        var data, gotData;
+        var data;
 
         if(stopped) {
           return;
@@ -84,57 +192,12 @@ window.MessageBus = (function() {
           setTimeout(poll, 500);
           return;
         }
+
         data = {};
         $.each(callbacks, function(_,callback) {
           data[callback.channel] = callback.last_id;
         });
-        gotData = false;
-        _this.longPoll = $.ajax(baseUrl + "message-bus/" + clientId + "/poll?" + (!shouldLongPoll() || !_this.enableLongPolling ? "dlp=t" : ""), {
-          data: data,
-          cache: false,
-          dataType: 'json',
-          type: 'POST',
-          headers: {
-            'X-SILENCE-LOGGER': 'true'
-          },
-          success: function(messages) {
-            failCount = 0;
-            $.each(messages,function(_,message) {
-              gotData = true;
-              $.each(callbacks, function(_,callback) {
-                if (callback.channel === message.channel) {
-                  callback.last_id = message.message_id;
-                  callback.func(message.data);
-                }
-                if (message.channel === "/__status") {
-                  if (message.data[callback.channel] !== undefined) {
-                    callback.last_id = message.data[callback.channel];
-                  }
-                }
-              });
-            });
-          },
-          error: failCount += 1,
-          complete: function() {
-            if (gotData) {
-              setTimeout(poll, 100);
-            } else {
-              interval = _this.callbackInterval;
-              if (failCount > 2) {
-                interval = interval * failCount;
-              } else if (!shouldLongPoll()) {
-                // slowning down stuff a lot when hidden
-                // we will need to add a lot of fine tuning here
-                interval = interval * 4;
-              }
-              if (interval > _this.maxPollInterval) {
-                interval = _this.maxPollInterval;
-              }
-              setTimeout(poll, interval);
-            }
-            _this.longPoll = null;
-          }
-        });
+        me.longPoll = longPoller(poll,data);
       };
       poll();
     },
@@ -154,8 +217,8 @@ window.MessageBus = (function() {
         func: func,
         last_id: lastId
       });
-      if (this.longPoll) {
-        return this.longPoll.abort();
+      if (me.longPoll) {
+        return me.longPoll.abort();
       }
     },
 
@@ -174,8 +237,8 @@ window.MessageBus = (function() {
           return callback.channel !== channel;
         }
       });
-      if (this.longPoll) {
-        return this.longPoll.abort();
+      if (me.longPoll) {
+        return me.longPoll.abort();
       }
     }
   };
