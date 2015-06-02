9f9370b042044ae1df0e611395b06c69246f18cb
Sam Saffron <sam.saffron@gmail.com>
2015-06-02 16:50:08 +1000

    Add basic presence to chat demo

--- examples/chat/chat.rb
+++ examples/chat/chat.rb
@@ -2,7 +2,20 @@ $LOAD_PATH.unshift File.expand_path('../../../lib', __FILE__)
 require 'message_bus'
 require 'sinatra'
 require 'sinatra/base'
+require 'set'
+require 'json'
 
+$redis = Redis.new
+
+$online = Hash.new
+MessageBus.subscribe "/presence" do |msg|
+  if user = msg.data["enter"]
+     $online[user] = Time.now
+  end
+  if user = msg.data["leave"]
+     $online.delete user
+  end
+end
 
 class Chat < Sinatra::Base
 
@@ -10,8 +23,24 @@ class Chat < Sinatra::Base
 
   use MessageBus::Rack::Middleware
 
+  post '/enter' do
+    name = params["name"]
+    i = 1
+    while $online.include? name
+      name = "#{params["name"]}#{i}"
+      i += 1
+    end
+    MessageBus.publish '/presence', {enter: params["name"]}
+    {users: $online.keys, name: name}.to_json
+  end
+
+  post '/leave' do
+    MessageBus.publish '/presence', {leave: params["name"]}
+  end
+
   post '/message' do
-    MessageBus.publish '/message', params
+    msg = {data: params["data"][0..500], name: params["name"][0..100]}
+    MessageBus.publish '/message', msg
 
     "OK"
   end
@@ -28,11 +57,23 @@ class Chat < Sinatra::Base
         #panel { position: fixed; bottom: 0; background-color: #FFFFFF; }
         #messages { padding-bottom: 40px; }
         .hidden { display: none; }
+        #users {
+         position: fixed; top: 0; right: 0; width: 160px; background-color: #fafafa; height: 100%;
+         border-left: 1px solid #dfdfdf;
+         padding: 5px;
+        }
+        #users ul li {  margin: 0; padding: 0; }
+        #users ul { list-style: none; margin 0; padding: 0; }
+        body { padding-right: 160px; }
     </style>
   </head>
   <body>
     <p>This is a trivial chat demo... It is implemented as a <a href="https://github.com/SamSaffron/message_bus/blob/master/examples/chat/chat.rb">Sinatra app</a>. The <a href="https://github.com/SamSaffron/message_bus">message_bus</a> can easily be added to any Rails/Rack app. <small>This app can be deployed with <a href="https://github.com/discourse/discourse_docker">Discourse Docker</a> using <a href="https://github.com/SamSaffron/message_bus/blob/master/examples/chat/docker_container/chat.yml">this template</a>.</small></p>
     <div id='messages' class="hidden"></div>
+    <div id='users' class="hidden">
+      <h3>Online</h3>
+      <ul></ul>
+    </div>
     <div id='panel' class="hidden">
       <form>
         <textarea cols=80 rows=2></textarea>
@@ -45,14 +86,53 @@ class Chat < Sinatra::Base
       $(function() {
         var name;
 
+        var enter = function(name, opts) {
+           if (opts && opts.check) {
+             var found = false;
+             $('#users ul li').each(function(){
+                found = found || ($(this).text().trim() === name.trim());
+                return !found;
+             });
+             if (found) { return; }
+           }
+
+           var li = $('<li></li>');
+           li.text(name);
+           $('#users ul').append(li);
+        };
+
         $('#messages, #panel').addClass('hidden');
 
         $('#your-name input').keyup(function(e){
           if(e.keyCode == 13) {
             name = $(this).val();
+            $.post("/enter", { name: name}, null, "json" ).success(function(data){
+              $.each(data.users,function(idx, name){
+                enter(name);
+              });
+              name = data.name;
+              $('#send').text("send (" + name + ")");
+              enter(name, {check: true});
+            });
             $('#your-name').addClass('hidden');
-            $('#messages, #panel').removeClass('hidden');
-            $('#send').text("send (" + name + ")");
+            $('#messages, #panel, #users').removeClass('hidden');
+
+            window.onbeforeunload = function(){
+              $.post("/leave", { name: name });
+            };
+
+            MessageBus.subscribe("/presence", function(msg){
+              if (msg.enter) {
+                 enter(msg.enter, {check: true});
+              }
+              if (msg.leave) {
+               $('#users ul li').each(function(){
+                  if ($(this).text() === msg.leave) {
+                    $(this).remove()
+                  }
+               });
+              }
+            });
           }
         });
 
@@ -70,7 +150,13 @@ class Chat < Sinatra::Base
           if (val.length === 0) {
             return;
           }
-          $.post("/message", { data: val, name: name} );
+
+          if (val.length > 500) {
+            alert("message too long");
+            return false;
+          } else {
+            $.post("/message", { data: val, name: name} );
+          }
           $('textarea').val("");
         };
 
