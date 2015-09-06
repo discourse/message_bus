e0d932134d37d9651724fa497e99c4aeb99b4183
Kane York <rikingcoding@gmail.com>
2015-09-06 15:03:39 -0700

    Add client tests for __flush and __status

--- spec/lib/client_spec.rb
+++ spec/lib/client_spec.rb
@@ -29,6 +29,43 @@ describe MessageBus::Client do
       log[0].data.should == 'world'
     end
 
+    it "should provide positions if client subscribes at -1" do
+      10.times do
+        @bus.publish('/hello', 'world')
+      end
+      @bus.publish('/shorter', 'channel')
+
+      @client.subscribe('/hello', '-1')
+      @client.subscribe('/shorter', '-1')
+
+      log = @client.backlog(allow_flush: true)
+
+      log.length.should == 1
+      log[0].channel.should == '/__status'
+      log[0].data.should == {
+          '/hello' => 10,
+          '/shorter' => 1
+      }
+    end
+
+    it "should deliver a flush message if the client is ahead" do
+      @bus.publish('/hello', 'world')
+      position = @bus.last_id('/hello')
+
+      @client.subscribe('/no_messages', nil)
+      @client.subscribe('/hello', 100)
+
+      log = @client.backlog(allow_flush: true)
+
+      log.length.should == 2
+      log[0].channel.should == '/__flush'
+      log[1].channel.should == '/__status'
+      log[1].data.should == {
+          '/no_messages' => 0,
+          '/hello' => 1
+      }
+    end
+
     it "allows only client_id in list if message contains client_ids" do
       @message = MessageBus::Message.new(1, 2, '/test', 'hello')
       @message.client_ids = ["1","2"]
