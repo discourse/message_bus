fd5689c4b82b0d34d858521240dff288970d6651
Sam Saffron <sam.saffron@gmail.com>
2016-01-05 21:08:45 +1100

    remove handlers from railties

--- lib/message_bus/rails/railtie.rb
+++ lib/message_bus/rails/railtie.rb
@@ -5,8 +5,6 @@ class MessageBus::Rails::Engine < ::Rails::Engine; end
 
 class MessageBus::Rails::Railtie < ::Rails::Railtie
   initializer "message_bus.configure_init" do |app|
-    MessageBus::MessageHandler.load_handlers("#{Rails.root}/app/message_handlers")
-
     # We want MessageBus to show up after the session middleware, but depending on how
     # the Rails app is configured that might be ActionDispatch::Session::CookieStore, or potentially
     # ActionDispatch::Session::ActiveRecordStore.
