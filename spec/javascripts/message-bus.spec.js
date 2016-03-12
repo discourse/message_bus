3ffbbb4d27e340d2d73fdf0096e43395d079a450
Nathan Stitt <nathan@stitt.org>
2016-03-12 14:04:50 -0600

    Test additional methods and config options

--- spec/javascripts/message-bus.spec.js
+++ spec/javascripts/message-bus.spec.js
@@ -2,9 +2,6 @@ describe("Messagebus", function() {
 
   it("submits change requests", function(done){
     spyOn(this.MockedXMLHttpRequest.prototype, 'send').and.callThrough();
-    this.responseChunks = [
-      {channel: '/test', data: 'hi'}
-    ];
     var spec = this;
     MessageBus.subscribe('/test', function(){
       expect(spec.MockedXMLHttpRequest.prototype.send)
@@ -17,19 +14,55 @@ describe("Messagebus", function() {
   });
 
   it("calls callbacks", function(done){
-    spyOn(this.MockedXMLHttpRequest.prototype, 'open').and.callThrough();
-    this.responseChunks = [
-      {channel: '/test', data: {password: 'MessagebusRocks!'}}
-    ];
-    var spec = this;
     MessageBus.subscribe('/test', function(message){
-      expect(spec.MockedXMLHttpRequest.prototype.open)
-        .toHaveBeenCalledWith('POST', jasmine.stringMatching(/message-bus\/\w{32}\/poll/) );
-      expect(message.password).toEqual('MessagebusRocks!');
+      expect(message.password).toEqual('MessageBusRocks!');
       done();
     });
   });
 
+  it('stores messages when paused, then delivers them when resumed', function(done){
+    MessageBus.pause()
+    spyOn(this.MockedXMLHttpRequest.prototype, 'send').and.callThrough();
+    var spec = this;
+    var onMessageSpy = jasmine.createSpy('onMessageSpy');
+    MessageBus.subscribe('/test', onMessageSpy);
+    setTimeout(function(){
+      expect(spec.MockedXMLHttpRequest.prototype.send).toHaveBeenCalled()
+      expect(onMessageSpy).not.toHaveBeenCalled()
+      MessageBus.resume()
+    }, 510) // greater than delayPollTimeout of 500
+    setTimeout(function(){
+      expect(onMessageSpy).toHaveBeenCalled()
+      done()
+    }, 550) // greater than first timeout above
+  });
 
+  it('can unsubscribe from callbacks', function(done){
+    var onMessageSpy = jasmine.createSpy('onMessageSpy');
+    MessageBus.subscribe('/test', onMessageSpy);
+    MessageBus.unsubscribe('/test', onMessageSpy);
+    MessageBus.subscribe('/test', function(){
+      expect(onMessageSpy).not.toHaveBeenCalled()
+      done()
+    });
+  });
+
+  testMB('sets dlp paramater when longPolling is disabled', function(){
+    MessageBus.enableLongPolling = false
+    this.perform(function(message, xhr){
+      expect(xhr.url).toMatch("dlp=t");
+    }).finally(function(){
+      MessageBus.enableLongPolling = true
+    })
+  });
+
+  testMB('respects baseUrl setting', function(){
+    MessageBus.baseUrl = "/a/test/base/url/";
+    this.perform(function(message, xhr){
+      expect(xhr.url).toMatch("/a/test/base/url/");
+    }).finally(function(){
+      MessageBus.baseUrl = "/";
+    })
+  });
 
 });
