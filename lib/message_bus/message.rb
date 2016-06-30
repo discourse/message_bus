e6b5b2eb965f0b5d7413a0adbf10aa25327b02cd
Sal Scotto <sal.scotto@gmail.com>
2016-06-30 14:02:52 -0600

    Changed MessageBus message form a struct to an object.
    Change MessageBus to a class instead of a Struct, I added some backwards compat mthod for struct member access.
    Done so that subscribers threads in development where class reloading is occurig in a seperate thread get module load/unload error about version.

--- lib/message_bus/message.rb
+++ lib/message_bus/message.rb
@@ -1,7 +1,14 @@
-class MessageBus::Message < Struct.new(:global_id, :message_id, :channel , :data)
-
-  attr_accessor :site_id, :user_ids, :group_ids, :client_ids
-
+class MessageBus::Message
+  INDEX_MAP={0=> :global_id, 1=> :message_id, 2=> :channel, 3=> :data}
+  attr_accessor :site_id, :user_ids, :group_ids, :client_ids, :global_id,:message_id,:channel,:data
+  
+  def initialize(_gi,_mi,_ch,_da)
+    @global_id = _gi
+    @message_id = _mi
+    @channel = _ch
+    @data = _da
+  end
+  
   def self.decode(encoded)
     s1 = encoded.index("|")
     s2 = encoded.index("|", s1+1)
@@ -14,5 +21,13 @@ class MessageBus::Message < Struct.new(:global_id, :message_id, :channel , :data
   # only tricky thing to encode is pipes in a channel name ... do a straight replace
   def encode
     global_id.to_s << "|" << message_id.to_s << "|" << channel.gsub("|","$$123$$") << "|" << data
+  end
+  
+  def[](key)
+    if key.is_a?(Integer)
+      self.send(INDEX_MAP[key])
+    else
+      self.send(key)  
+    end  
   end
 end
