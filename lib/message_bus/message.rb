b5a6420df3e1ec155336adebfdeb5211baa867f6
James Cook <James Cook>
2017-11-07 19:11:53 -0600

    Use gsub! instead of gsub

--- lib/message_bus/message.rb
+++ lib/message_bus/message.rb
@@ -11,7 +11,8 @@ class MessageBus::Message < Struct.new(:global_id, :message_id, :channel , :data
 
     global_id  = encoded[0, s1+1].to_i
     message_id = encoded[(s1 + 1), (s2 - s1 - 1)].to_i
-    channel    = encoded[(s2 + 1), (s3 - s2 - 1)].gsub("$$123$$", "|")
+    channel    = encoded[(s2 + 1), (s3 - s2 - 1)]
+    channel.gsub!("$$123$$", "|")
     data       = encoded[(s3 + 1), encoded.size]
 
     MessageBus::Message.new(global_id, message_id, channel, data)
