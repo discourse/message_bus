1733fd2a0050a8e881164cf4261d93840c418625
Jeremy Evans <code@jeremyevans.net>
2018-03-09 15:25:06 -0800

    Fix using global MessageBus, always use the related instance
    If you are using MessageBus::Instance to create separate instances,
    none of that code should call the singleton MessageBus instance.
    This fixes all cases where the singleton was used.
    
    In order to implement this, MessageBus::Diagnostics.enable needs
    to take an argument for the bus to use.  To retain backwards
    compatibility, this argument defaults to MessageBus.

--- lib/message_bus/backends/memory.rb
+++ lib/message_bus/backends/memory.rb
@@ -277,7 +277,7 @@ class MessageBus::Memory::ReliablePubSub
         end
       end
     rescue => error
-      MessageBus.logger.warn "#{error} subscribe failed, reconnecting in 1 second. Call stack\n#{error.backtrace.join("\n")}"
+      @config[:logger].warn "#{error} subscribe failed, reconnecting in 1 second. Call stack\n#{error.backtrace.join("\n")}"
       sleep 1
       retry
     end
